fc17d5bab34e277ca1f97d2b24b2e1b70a7a4cbc
jeremieP <jeremie.pereira@gmail.com>
2023-12-22 22:20:08 +0100

    first cubes + camera

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,38 @@
-console.log('Hello World! from index.js')
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const greenMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const redMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const blueMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff })
+const greenCube = new THREE.Mesh(geometry, greenMaterial)
+
+scene.add(greenCube)
+
+const redCube = new THREE.Mesh(geometry, redMaterial)
+const blueCube = new THREE.Mesh(geometry, blueMaterial)
+
+redCube.position.x = -1
+redCube.position.y = 1
+blueCube.position.x = 1
+blueCube.position.y = -1
+
+scene.add(redCube)
+scene.add(blueCube)
+
+const sizes = {
+  width: 800,
+  height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+const canvas = document.querySelector('canvas.webgl')
+const renderer = new THREE.WebGLRenderer({
+  canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
